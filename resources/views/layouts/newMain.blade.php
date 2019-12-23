bfd4a575cea6e5fc2dcd649375691d1b3b396fb2
frigginglorious <frigginglorious@gmail.com>
2019-12-23 07:11:11 +0000

    icon fuckery

--- resources/views/layouts/newMain.blade.php
+++ resources/views/layouts/newMain.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.SITE_NAME') }}</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" type="text/css" href="/css/fontawesome.min.css">
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
@@ -19,7 +20,7 @@
 	{{-- <link rel="stylesheet" type="text/css" href="/css/bootstrapEdited.css"> --}}
 
 		<link rel="stylesheet" type="text/css" href="/css/custom.css">
-    <link rel="stylesheet" type="text/css" href="/css/fontawesome.min.css">
+    
 	<link rel="stylesheet" type="text/css" href="/css/all.min.css">
 
 	
